614deffde05d6c1bfc211a80447c76b3a631ffce
James Scott <jscott@jscott10.com>
2015-10-04 21:45:33 -0400

    Refactoring and comments; lives broken

--- js/app.js
+++ js/app.js
@@ -11,44 +11,47 @@ var ENEMY_START_X = -101, // Initial x-coord (left) of Enemy
 	PLAYER_MOVE_X = 101, // Abs value of x-displacement (left-arrow, right-arrow)
 	PLAYER_MOVE_Y = 84; // Abs value of y-displacement (up-arrow, down-arrow)
 
-// Randomize enemy speed:
-
-// ENEMY_BASE_SPEED:
-function randomEnemySpeed() {
-	return Math.floor((Math.random() * (ENEMY_MAX_SPEED - ENEMY_MIN_SPEED) + ENEMY_MIN_SPEED)) * 10;
-}
-
 // Enemies our player must avoid
 var Enemy = function() {
 
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
+    this.loc = {x: 0, y: 0};
+
+    this.init();
+
     // Set Enemy x-coord
-	this.loc = {
-		x: ENEMY_START_X,
-	};
+	// this.loc = {
+	// 	x: ENEMY_START_X,
+	// };
 
 	// Set random x-movement
-	this.v = randomEnemySpeed();
+	// this.v = randomEnemySpeed();
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
+// Position Enemy at start of row and randomize speed.
+Enemy.prototype.init = function() {
+	this.loc.x = ENEMY_START_X;
+	this.v = randomEnemySpeed();
+}
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+	// You should multiply any movement by the dt parameter
+	// which will ensure the game runs at the same speed for
+	// all computers.
 	this.loc.x += (dt * this.v);
-	// If Enemy moves beyond end of row, wrap to beginning
-	if(this.loc.x > ENEMY_END_X) {
-		this.wrap();
-	}
 
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+	// If Enemy moves beyond end of row, wrap to beginning and randomize speed
+	if(this.loc.x > ENEMY_END_X) {
+		this.init();
+	}
 };
 
 // Draw the enemy on the screen, required method for game
@@ -56,14 +59,10 @@ Enemy.prototype.render = function() {
 	ctx.drawImage(Resources.get(this.sprite), this.loc.x, this.loc.y);
 };
 
-// Position Enemy at start of row and randomize speed.
-Enemy.prototype.wrap = function() {
-	this.loc.x = ENEMY_START_X;
-	this.v = randomEnemySpeed();
+// Randomize enemy speed:
+function randomEnemySpeed() {
+	return Math.floor((Math.random() * (ENEMY_MAX_SPEED - ENEMY_MIN_SPEED) + ENEMY_MIN_SPEED)) * 10;
 }
-
-
-
 
 
 // Now write your own player class
@@ -86,6 +85,7 @@ Player.prototype.initGame = function() {
 		x: PLAYER_START_X,
 		y: PLAYER_START_Y
 	};
+
 	this.target = {
 		x: PLAYER_START_X,
 		y: PLAYER_START_Y
@@ -101,6 +101,7 @@ Player.prototype.initTurn = function() {
 		x: PLAYER_START_X,
 		y: PLAYER_START_Y
 	};
+
 	this.target = {
 		x: PLAYER_START_X,
 		y: PLAYER_START_Y
@@ -108,6 +109,8 @@ Player.prototype.initTurn = function() {
 
 };
 
+// Move the Player in the direction indicated
+// Player cannot move beyond the edges of the grid
 Player.prototype.move = function(direction) {
 	switch(direction) {
 		case 'left':
@@ -134,55 +137,57 @@ Player.prototype.move = function(direction) {
 	console.log("x: "+this.loc.x + " y: "+this.loc.y);
 }
 
+// Update the Player state:
 Player.prototype.update = function(dt) {
-	// Made it!
+
+	//	If Player made it to top row, increment score, display score and reset to start position
 	if(this.loc.y === -11) {
 		this.updateScore(10);
 		displayScore(this.score);
 		this.loc.x = PLAYER_START_X;
 		this.loc.y = PLAYER_START_Y;
 	}
-	// Detect enemy collision
-	allEnemies.forEach(function (enemy, index, arr) {
-		// var w = 101;
-		// console.log(enemy);
-		// console.log(index);
-		// console.log(arr);
-		// console.log("e: "+enemy.loc.y);
-		// console.log("p: "+this.loc.y);
-		if ((enemy.loc.y == this.loc.y-11) && (enemy.loc.x-ENEMY_WIDTH+17 < this.loc.x && enemy.loc.x+ENEMY_WIDTH-17 > this.loc.x)) {
-			this.initTurn();
+
+	// If Player collides with an Enemy, display score and reset to start position
+	allEnemies.forEach(function (enemy) {
+		if( (enemy.loc.y == this.loc.y - 11) && 
+			((enemy.loc.x - ENEMY_WIDTH + 17 < this.loc.x) && (enemy.loc.x + ENEMY_WIDTH - 17 > this.loc.x)) ) {
 			displayScore(this.score);
+			this.initTurn();
 		}
 	},this);
 
-	// Detect star collision
-	if ((star instanceof Star) &&
-	    (star.loc.y == this.loc.y-11) &&
-	    (star.loc.x-ENEMY_WIDTH < this.loc.x && star.loc.x+ENEMY_WIDTH > this.loc.x)) {
+	// If Player collides with a Star, increment score, display score and destroy the Star object
+	if( (star instanceof Star) && ((star.loc.x === this.loc.x) && (star.loc.y === (this.loc.y - 11)))) {
 			this.updateScore(40);
 			displayScore(this.score);
 			star = null;
 	}
-
 };
 
+// Increment the score
 Player.prototype.updateScore = function(score) {
 	this.score += score;
 }
 
+// Render the Player at x,y
 Player.prototype.render = function(dt) {
 	ctx.drawImage(Resources.get(this.sprite), this.loc.x, this.loc.y);
 };
 
+// Move the Player based on keyboard input
 Player.prototype.handleInput = function(key) {
 	this.move(key);
 };
 
+
+// Star implements a randomly appearing power-up
 var Star = function() {
 
+	// Lifetime of the Star (5 seconds)
 	this.timer = 5;
 
+	// Random location of the Star
 	this.loc = {
 		x: Math.floor(Math.random() * 5) * 101,
 		y: ROW_Y[Math.floor(Math.random() * 3)]
@@ -191,11 +196,12 @@ var Star = function() {
     this.sprite = 'images/Star.png';
 };
 
+// Decrement counter by time delta
 Star.prototype.countdown = function(dt) {
 	this.timer -= dt;
-//	console.log(this.timer);
 };
 
+// Decrement the counter if gt 0
 Star.prototype.update = function(dt) {
 	if(this.timer > 0) {
 		this.countdown(dt);
@@ -212,16 +218,20 @@ Star.prototype.render = function(dt) {
 
 var allEnemies = new Array();
 
-for(i=0; i<3; i++) {
-	var enemy = new Enemy();
-	enemy.loc.y = ROW_Y[i];
-	allEnemies[i] = enemy;
-}
+initializeEnemyArray();
 
 var player = new Player();
 
 var star = new Star();
 
+function initializeEnemyArray() {
+	for(i=0; i<3; i++) {
+		allEnemies[i] = new Enemy();
+		allEnemies[i].loc.y = ROW_Y[i];
+	}
+}
+
+// Display the score and lives remaining at the top of the screen
 function displayScore(score) {
 	ctx.font = "20px Georgia";
 	ctx.clearRect(0,0,400,30);
